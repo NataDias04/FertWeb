af2f77146045764d910299d988aeaaf4b97366fd
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-03-31 20:15:32 -0300

    Update EnvioDeDados.js

--- FRONTEND/EnvioDeDados.js
+++ FRONTEND/EnvioDeDados.js
@@ -4,7 +4,7 @@ const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq
     password: '12345678'
 });
 
-const topic = 'meu/topico';
+const topic = 'Raspberry/Api';
 
 client.on('connect', function () {
     console.log('Conectado ao broker MQTT');
