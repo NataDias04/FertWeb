84a1fb59ead30ccf7aab6c36d0974e15f8966e5d
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-04-07 20:00:01 -0300

    Update FrontEnd.js

--- FRONTEND/FrontEnd.js
+++ FRONTEND/FrontEnd.js
@@ -1,1 +1,29 @@
+const data = {
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+    datasets: [{
+        label: 'Vendas',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: 'cornflowerblue',
+        borderColor: 'cornflowerblue',
+        borderWidth: 1
+    }]
+};
 
+// Opções do gráfico (opcional)
+const options = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+};
+
+// Contexto do canvas
+const ctx = document.getElementById('meuGrafico').getContext('2d');
+
+// Inicializando o gráfico
+const myChart = new Chart(ctx, {
+    type: 'bar', // Tipo de gráfico (bar, line, pie, etc.)
+    data: data,
+    options: options
+});
