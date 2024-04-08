9b2bbfbe6680f097699f8f2568605d8c415b9ba1
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-04-08 19:08:04 -0300

    Update EnvioDeDados.js

--- FRONTEND/EnvioDeDados.js
+++ FRONTEND/EnvioDeDados.js
@@ -1,4 +1,4 @@
-const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt', {
+/*const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt', {
     clientId: '123projetopi',
     username: 'projetopi',
     password: '12345678'
@@ -40,6 +40,49 @@ function exibirConfirmacao(mensagem) {
     const confirmacaoElement = document.getElementById('confirmacao');
     confirmacaoElement.textContent = mensagem;
 }
+
+function exibirErro(erro) {
+    const erroElement = document.getElementById('erro');
+    erroElement.textContent = erro;
+    erroElement.style.color = 'red'; // Define a cor do texto para vermelho para indicar erro
+} */
+
+const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt', {
+    clientId: '123projetopi',
+    username: 'projetopi',
+    password: '12345678'
+});
+
+const topic = 'Api/Raspberry';
+
+client.on('connect', function () {
+    console.log('Conectado ao broker MQTT');
+
+    client.subscribe(topic, function (err) {
+        if (err) {
+            console.error('Erro ao se inscrever no tópico', err);
+        } else {
+            console.log('Inscrição no tópico bem-sucedida');
+        }
+    });
+});
+
+function enviarMensagem() {
+    const mensagem = 'Olá, Raspberry Pi!'; // Defina a mensagem que deseja enviar
+    client.publish(topic, mensagem, function (err) {
+        if (err) {
+            console.error('Erro ao enviar mensagem:', err);
+            exibirErro('Erro ao enviar mensagem: ' + err);
+        } else {
+            console.log('Mensagem enviada com sucesso:', mensagem);
+        }
+    });
+}
+
+document.getElementById("botao-enviar").addEventListener("click", function() {
+    // Aqui você chama a função para enviar a mensagem para a Raspberry Pi
+    enviarMensagem();
+});
 
 function exibirErro(erro) {
     const erroElement = document.getElementById('erro');
