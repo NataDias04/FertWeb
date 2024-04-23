e623f0d3aba2ee71fe8ae186dba3458577a4457f
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-04-23 18:44:21 -0300

    Update grafico.py

--- FRONTEND/grafico.py
+++ FRONTEND/grafico.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
-# Obter o token de acesso pessoal da variável de ambiente
-token = os.environ.get('GITHUB_TOKEN')
-
 # Dados de exemplo - Temperatura do solo ao longo do tempo
 tempo = 18 #np.arange(0, 24, 1)  # Horas do dia
 temperatura =  8 #np.random.randint(15, 25, size=len(tempo))  # Temperatura aleatória entre 15°C e 25°C
@@ -24,26 +21,50 @@ plt.savefig('grafico_temperatura_solo.png')
 
 print("Gráfico de temperatura do solo foi criado e salvo como 'grafico_temperatura_solo.png'.")
 
-# Fazer upload do arquivo para o repositório do GitHub
+import base64
+
+# Configurações do repositório e do arquivo
 github_repo = 'NataDias04/FertWeb'
 file_path = 'FRONTEND/grafico_temperatura_solo.png'
+commit_message = 'Atualizando gráfico de temperatura do solo'
+
+# Obter o token de acesso pessoal da variável de ambiente
+token = os.environ.get('GITHUB_TOKEN')
+
+# URL para obter informações sobre o arquivo
 github_url = f'https://api.github.com/repos/{github_repo}/contents/{file_path}'
 
+# Ler o conteúdo do arquivo
 with open(file_path, 'rb') as file:
     file_content = file.read()
 
-headers = {
-    'Authorization': f'token {token}'
+# Codificar o conteúdo do arquivo em base64
+file_content_base64 = base64.b64encode(file_content).decode('utf-8')
+
+# Obter o SHA do arquivo atual no repositório
+response = requests.get(github_url)
+current_file_info = response.json()
+current_sha = current_file_info['sha']
+
+# Construir os dados para a requisição de commit
+commit_data = {
+    'message': commit_message,
+    'content': file_content_base64,
+    'sha': current_sha,
+    'committer': {
+        'name': 'Nata',
+        'email': 'natadiassantos@ucl.br'
+    }
 }
 
-data = {
-    'message': 'Atualizando gráfico de temperatura do solo',
-    'content': file_content
-}
+# URL para realizar o commit
+commit_url = f'https://api.github.com/repos/{github_repo}/contents/{file_path}'
 
-response = requests.put(github_url, headers=headers, json=data)
+# Realizar o commit
+response = requests.put(commit_url, headers={'Authorization': f'token {token}'}, json=commit_data)
 
+# Verificar o resultado do commit
 if response.status_code == 200:
-    print('Gráfico de temperatura do solo foi atualizado no repositório.')
+    print('Commit realizado com sucesso.')
 else:
-    print('Erro ao atualizar o gráfico no repositório:', response.text)
+    print('Erro ao realizar o commit:', response.text)
