235c56e85dad9948ea2a298352694046c8e693fb
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-04-23 16:57:57 -0300

    Update grafico.py

--- FRONTEND/grafico.py
+++ FRONTEND/grafico.py
@@ -3,8 +3,8 @@ import numpy as np
 import requests
 
 # Dados de exemplo - Temperatura do solo ao longo do tempo
-tempo = 5 #np.arange(0, 24, 1)  # Horas do dia
-temperatura =  40#np.random.randint(15, 25, size=len(tempo))  # Temperatura aleatória entre 15°C e 25°C
+tempo = np.arange(0, 24, 1)  # Horas do dia
+temperatura =  np.random.randint(15, 25, size=len(tempo))  # Temperatura aleatória entre 15°C e 25°C
 
 # Criando o gráfico
 plt.figure(figsize=(10.9, 2.50))  # Define o tamanho do gráfico conforme solicitado
