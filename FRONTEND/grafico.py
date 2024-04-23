8f10caaf82c03679669e3784f066fa55acf9209d
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-04-23 16:56:30 -0300

    Update grafico.py

--- FRONTEND/grafico.py
+++ FRONTEND/grafico.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import requests
 
 # Dados de exemplo - Temperatura do solo ao longo do tempo
 tempo = 10 #np.arange(0, 24, 1)  # Horas do dia
@@ -18,3 +19,23 @@ plt.tight_layout()
 plt.savefig('grafico_temperatura_solo.png')
 
 print("Gráfico de temperatura do solo foi criado e salvo como 'grafico_temperatura_solo.png'.")
+
+# Fazer upload do arquivo para o repositório do GitHub
+github_repo = 'NataDias04/FertWeb'
+file_path = 'FRONTEND/grafico_temperatura_solo.png'
+github_url = f'https://api.github.com/repos/{github_repo}/contents/{file_path}'
+
+with open(file_path, 'rb') as file:
+    file_content = file.read()
+
+data = {
+    'message': 'Atualizando gráfico de temperatura do solo',
+    'content': file_content
+}
+
+response = requests.put(github_url, json=data)
+
+if response.status_code == 200:
+    print('Gráfico de temperatura do solo foi atualizado no repositório.')
+else:
+    print('Erro ao atualizar o gráfico no repositório:', response.text)
