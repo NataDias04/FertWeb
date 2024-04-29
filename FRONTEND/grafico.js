a83822ab1c2c44a088e37c383c1100745446ad23
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-04-29 19:33:02 -0300

    Update grafico.js

--- FRONTEND/grafico.js
+++ FRONTEND/grafico.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
     var options1 = {
       scales: {
-        yAxes: [{
+        yAxis: [{
           ticks: {
             beginAtZero: true
           }
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
     var options2 = {
       scales: {
-        yAxes: [{
+        yAxis: [{
           ticks: {
             beginAtZero: true
           }
