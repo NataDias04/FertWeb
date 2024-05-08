b04a1912a44b0b9febe7d49a6b10f0308c422fb1
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-07 21:49:13 -0300

    Update EnvioDeDados.js

--- FRONTEND/EnvioDeDados.js
+++ FRONTEND/EnvioDeDados.js
@@ -1,8 +1,8 @@
-const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt', {
+/*const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt', {
     clientId: '123projetopi',
     username: 'projetopi',
     password: '12345678'
-});
+});*/
 
 const topic = 'Api/Raspberry';
 
