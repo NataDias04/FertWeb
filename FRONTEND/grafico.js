f388b8b4a6b1e9c91627eddfe155c5d87764a033
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-11 00:48:38 -0300

    Update grafico.js

--- FRONTEND/grafico.js
+++ FRONTEND/grafico.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
     // Primeiro gráfico: Temperatura diária
+    
     var data1 = {
       labels: ["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00",
                 "11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"],
@@ -29,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var indiceAtual = 0;
 
-window.adicionarMensagemAoGrafico = function(mensagem) {
+    window.adicionarMensagemAoGrafico = function(mensagem) {
 
         if (indiceAtual === 0) {
             
@@ -49,9 +51,10 @@ window.adicionarMensagemAoGrafico = function(mensagem) {
         if (indiceAtual === 24) {
             indiceAtual = 0;
         }
-}
+    }
   
-    // Segundo gráfico: Médias do ano
+    // Segundo gráfico: Médias da semana
+    
     var data2 = {
       labels: ["seg.","ter.","qua.","qui.","sex.","sab.","dom."],
       datasets: [{
@@ -60,7 +63,7 @@ window.adicionarMensagemAoGrafico = function(mensagem) {
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
-        data: [15, 16, 17, 18, 20, 22, 24] // Dados de exemplo
+        data: [15, 16, 17, 18, 20, 22, 24]
       }]
     };
   
@@ -79,34 +82,32 @@ window.adicionarMensagemAoGrafico = function(mensagem) {
       options: options2
     });    
 
+    // Terceiro gráfico: Médias do ano
+    
     var data3 = {
-  labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
-  datasets: [{
-    label: "Temperatura Média °C",
-    fill: false,
-    backgroundColor: "rgba(54, 162, 235, 0.2)",
-    borderColor: "rgba(54, 162, 235, 1)",
-    borderWidth: 1,
-    data: [18, 19, 20, 21, 22, 23, 24, 23, 22, 20, 19, 18] // Dados de exemplo
-  }]
-};
+      labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+      datasets: [{
+        label: "Temperatura Média °C",
+        fill: false,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+        data: [18, 19, 20, 21, 22, 23, 24, 23, 22, 20, 19, 18] 
+        }]
+    };
 
-var options3 = {
-  scales: {
-    y: [{
-        beginAtZero: true
-    }]
-  }
-};
-
-var ctx3 = document.getElementById('MediasDoAno').getContext('2d');
-var myChart3 = new Chart(ctx3, {
-  type: 'line',
-  data: data3,
-  options: options3
+    var options3 = {
+      scales: {
+        y: [{
+            beginAtZero: true
+        }]
+      }
+    };
+    
+    var ctx3 = document.getElementById('MediasDoAno').getContext('2d');
+    var myChart3 = new Chart(ctx3, {
+      type: 'line',
+      data: data3,
+      options: options3
+    });
 });
-  });
-
-window.testeDeErro = function() {
-    console.log("deu certo!!");
-};
