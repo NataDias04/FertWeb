65780aafbd587f762e535af409e0f7ab48402459
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-11 00:37:51 -0300

    Update RecebedorDeDados.js

--- FRONTEND/RecebedorDeDados.js
+++ FRONTEND/RecebedorDeDados.js
@@ -1,29 +1,8 @@
-const client = mqtt.connect('wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt', {
-    clientId: '123projetopi',
-    username: 'projetopi',
-    password: '12345678'
-});
-
+const mqttClient = require('./mqttClient');
 const topic = 'Raspberry/Api';
 let ultimaMensagem = '';
-let isConnected = false; // Variável para rastrear o estado da conexão
-
-client.on('connect', function () {
-    if (!isConnected) { // Verifica se ainda não está conectado
-        isConnected = true; // Atualiza o estado da conexão
-        console.log('Conectado ao broker MQTT Raspberry/Api');
-
-        client.subscribe(topic, function (err) {
-            if (err) {
-                console.error('Erro ao se inscrever no tópico Raspberry/Api', err);
-            } else {
-                console.log('Inscrição no tópico bem-sucedida Raspberry/Api');
-            }
-        });
-    }
-});
-
 let CodigoDaUltimaMensagem = "";
+let isConnected = false;
 
 client.on('message', function (receivedTopic, message) {
 
@@ -32,7 +11,6 @@ client.on('message', function (receivedTopic, message) {
     if(CodigoDaNovaMensagem !== CodigoDaUltimaMensagem){
         console.log('Mensagem recebida no tópico', receivedTopic, ':', message.toString());
         exibirUltimaMensagemNaPagina();
-        //testeDeErro();
         adicionarMensagemAoGrafico( message.toString());
         CodigoDaUltimaMensagem = CodigoDaNovaMensagem;
     }  
@@ -41,10 +19,8 @@ client.on('message', function (receivedTopic, message) {
 function exibirUltimaMensagemNaPagina() {
     const listaMensagens = document.getElementById('mensagens');
     
-    // Limpar mensagens antigas
     listaMensagens.innerHTML = '';
 
-    // Criar novo item de lista com a última mensagem
     const novaMensagem = document.createElement('li');
     novaMensagem.textContent = ultimaMensagem;
     listaMensagens.appendChild(novaMensagem);
