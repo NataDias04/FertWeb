30b99f968718d0ff6eae417329802e63e78a675a
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-11 00:44:51 -0300

    Update mqttClient.js

--- FRONTEND/mqttClient.js
+++ FRONTEND/mqttClient.js
@@ -12,3 +12,6 @@ const mqttConfig = {
 const mqttClient = mqtt.connect(mqttConfig);
 
 module.exports = mqttClient;
+
+
+/*client.on('connect', function () { if (!isConnected) { // Verifica se ainda não está conectado isConnected = true; // Atualiza o estado da conexão console.log('Conectado ao broker MQTT Raspberry/Api'); client.subscribe(topic, function (err) { if (err) { console.error('Erro ao se inscrever no tópico Raspberry/Api', err); } else { console.log('Inscrição no tópico bem-sucedida Raspberry/Api'); } }); } });*/
