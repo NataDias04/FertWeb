f97de8b4237b0cad97e88262b061746e847dd554
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-13 01:22:52 -0300

    Update mqttClient.js

--- FRONTEND/mqttClient.js
+++ FRONTEND/mqttClient.js
@@ -2,7 +2,7 @@ let isConnected = false;
 
 const mqttConfig = {
     host: 'wss://4b8d62e12d9744ec8e2fdea77a5e66e2.s1.eu.hivemq.cloud:8884/mqtt',
-    clientId: '123projetopi',
+    clientId: 'projetopi333',
     username: 'projetopi',
     password: '12345678'
 };
