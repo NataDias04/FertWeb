be269f70537a30f164c98c6e42c91ce1ae4c5519
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-21 20:32:15 -0300

    Update RecebedorDeDados.cs

--- BACKEND/RecebedorDeDados.cs
+++ BACKEND/RecebedorDeDados.cs
@@ -5,6 +5,9 @@ using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
 
+using System.Device.Gpio;
+using System.Threading;
+
 class RecebedorDeDados
 {
     public static async Task IniciarRecebimento()
@@ -19,10 +22,21 @@ class RecebedorDeDados
             .WithCredentials("projetopi", "12345678")
             .Build();
 
+        // Definir o pino do LED
+        int pin = 6;
+
+        // Criar uma instância do controlador GPIO
+        using var controller = new GpioController();
+
+        // Abrir o pino do LED como saída
+        controller.OpenPin(pin, PinMode.Output);
+
+        controller.Write(pin, PinValue.Low);
+
         mqttClient.UseConnectedHandler(async e =>
         {
             Console.WriteLine("Conectado ao broker MQTT. api>>rasp");
-            await mqttClient.SubscribeAsync("Api/Raspberry"); 
+            await mqttClient.SubscribeAsync("Api/Raspberry");
         });
 
         mqttClient.UseDisconnectedHandler(async e =>
@@ -34,10 +48,24 @@ class RecebedorDeDados
 
         mqttClient.UseApplicationMessageReceivedHandler(e =>
         {
-            Console.WriteLine($"Mensagem recebida no tópico '{e.ApplicationMessage.Topic}': {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            string mensagem = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            Console.WriteLine($"Mensagem recebida no tópico '{e.ApplicationMessage.Topic}': {mensagem}");
+
+            // Verificar se a mensagem é a esperada para ligar o LED
+            if (mensagem == "Olá, Raspberry Pi!")
+            {
+                // Ativar o LED
+                controller.Write(pin, PinValue.High);
+                
+                // Esperar por 5 segundos
+                Thread.Sleep(5000);
+                
+                // Desativar o LED
+                controller.Write(pin, PinValue.Low);
+            }
         });
 
         await mqttClient.ConnectAsync(options);
-        Console.ReadLine();
+        Console.ReadLine(); // Aguardar até que o programa seja encerrado
     }
 }
