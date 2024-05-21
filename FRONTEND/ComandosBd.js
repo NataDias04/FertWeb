3b6aab988b36d893eac0ace98ba4ec492f3519a6
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-21 14:09:57 -0300

    Update ComandosBd.js

--- FRONTEND/ComandosBd.js
+++ FRONTEND/ComandosBd.js
@@ -3,18 +3,13 @@ import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/10.4.0
 
 // Função para adicionar temperatura ao banco de dados
 export function adicionarTemperatura(temperatura) {
-    // Obtém uma referência para o banco de dados
     const database = getDatabase();
+    const timestamp = new Date().toISOString().replace(/[.#$/[\]]/g, '_'); // Substitui caracteres inválidos
 
-    // Obtém o timestamp atual
-    const timestamp = new Date().toISOString();
-
-    // Define os dados a serem gravados
     const data = {
         valor: temperatura,
         timestamp: timestamp
     };
 
-    // Grava os dados no banco de dados
     return set(ref(database, 'temperaturas/' + encodeURIComponent(timestamp)), data);
 }
