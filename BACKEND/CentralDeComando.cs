c511b7632a781aae8278e75b028225c962cffa8f
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-05-21 20:41:31 -0300

    Update CentralDeComando.cs

--- BACKEND/CentralDeComando.cs
+++ BACKEND/CentralDeComando.cs
@@ -7,21 +7,16 @@ class CentralDeComando
     {
         //Console.WriteLine("Iniciando envio de dados...");
         //await EnvioDeDados.InicializarEnvio();
-
         //Console.WriteLine("Iniciando recebimento de dados...");
         //await RecebedorDeDados.IniciarRecebimento();
-
         Console.WriteLine("Iniciando envio e recebimento de dados...");
         // Iniciar as duas tarefas simultaneamente
         var envioTask = EnvioDeDados.InicializarEnvio();
         var recebimentoTask = RecebedorDeDados.IniciarRecebimento();
-
         // Aguardar a conclusão de ambas as tarefas
         await Task.WhenAll(envioTask, recebimentoTask);
-
         //Manter o servio funcionando
         await Task.Delay(-1);
-
         Console.ReadLine();
     }
 }
