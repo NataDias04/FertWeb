8f1169d4535c4267f4bec296aa4ed8d15d64cc12
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-06-04 17:32:52 -0300

    Update GraficosTempAmbiente.js

--- FRONTEND/GraficosTempAmbiente.js
+++ FRONTEND/GraficosTempAmbiente.js
@@ -97,14 +97,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var indiceAtualAno = 0;
 
     // Funções
-    var isIndexPage = document.getElementById('mensagens-temperatura') !== null;
     
     window.adicionarMensagemAoGrafico = function (mensagem) {
-        if (isIndexPage) {
-            if (indiceAtual === 0) {
-                graficoTemperaturaDiaria.data.datasets[0].data = Array(24).fill(null);
-                graficoTemperaturaDiaria.update();
-            }
+        if (indiceAtual === 0) {
+             graficoTemperaturaDiaria.data.datasets[0].data = Array(24).fill(null);
+             graficoTemperaturaDiaria.update();
         }
         var dadosAtuais = graficoTemperaturaDiaria.data.datasets[0].data;
         dadosAtuais[indiceAtual] = parseInt(mensagem);
