db2b06d59a4570d34aae5778648e18d9327e3784
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-06-04 01:00:16 -0300

    Update GraficoNPK.js

--- FRONTEND/GraficoNPK.js
+++ FRONTEND/GraficoNPK.js
@@ -1,34 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    const dadosNPK = {
-        labels: ['Nitrogênio', 'Fósforo', 'Potássio'],
-        datasets: [{
-            label: 'Nutrição do Solo NPK',
-            data: [30, 40, 30], // Valores percentuais para cada nutriente
-            backgroundColor: [
-                'rgb(255, 99, 132)', // Cor para Nitrogênio
-                'rgb(54, 162, 235)', // Cor para Fósforo
-                'rgb(255, 205, 86)' // Cor para Potássio
-            ],
-            hoverOffset: 4
-        }]
-    };
-
-    const config = {
-        type: 'doughnut',
-        data: dadosNPK,
-        options: {
-            aspectRatio: 1,
-            responsive: false
-        }
-    };
-
-    var graficoNutricaoSolo = new Chart(
-        document.getElementById('graficoNutricaoSolo'),
-        config
-    );
-});*/
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const dadosNutricaoSolo = {
         labels: ['Nitrogênio', 'Fósforo', 'Potássio'],
