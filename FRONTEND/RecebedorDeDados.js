37dae0ce0ad1bbcba3d850c2c746a9e1d6f70338
Natã Dias dos Santos <142185726+NataDias04@users.noreply.github.com>
2024-06-03 23:48:17 -0300

    Update RecebedorDeDados.js

--- FRONTEND/RecebedorDeDados.js
+++ FRONTEND/RecebedorDeDados.js
@@ -54,54 +54,63 @@ const topico = 'Raspberry/Api';
 
 let ultimaMensagem = '';
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Subscrever ao tópico
+    ClienteMqtt.subscribe(topico, function (erro) {
+        if (erro) {
+            console.error('Erro ao se inscrever no tópico', erro);
+        } else {
+            console.log('Inscrição no tópico bem-sucedida');
+        }
+    });
 
-// Subscrever ao tópico
-ClienteMqtt.subscribe(topico, function (erro) {
-    if (erro) {
-        console.error('Erro ao se inscrever no tópico', erro);
-    } else {
-        console.log('Inscrição no tópico bem-sucedida');
+    // Callback para mensagens recebidas
+    ClienteMqtt.on('message', function (recebidoDoTopico, mensagem) {
+        console.log('Mensagem recebida no tópico', recebidoDoTopico, ':', mensagem.toString());
+        ultimaMensagem = mensagem.toString();
+        const [temperatura, umidade] = ultimaMensagem.split(':');
+        exibirTemperaturaNaPagina(temperatura);
+        exibirUmidadeNaPagina(umidade);
+        adicionarMensagemAoGrafico(temperatura, umidade);
+        const chaveTemperatura = adicionarTemperatura(temperatura);
+        console.log("Temperatura adicionada com sucesso. Chave gerada:", chaveTemperatura);
+    });
+
+    // Funções
+
+    function exibirTemperaturaNaPagina(temperatura) {
+        const listaMensagens = document.getElementById('mensagens-temperatura');
+        if (listaMensagens) {
+            listaMensagens.innerHTML = '';
+            const novaMensagem = document.createElement('li');
+            novaMensagem.textContent = `${temperatura}ºC`;
+            listaMensagens.appendChild(novaMensagem);
+        } else {
+            console.error('Elemento mensagens-temperatura não encontrado.');
+        }
     }
-});
 
-// Callback para mensagens recebidas
-ClienteMqtt.on('message', function (recebidoDoTopico, mensagem) {
-    console.log('Mensagem recebida no tópico', recebidoDoTopico, ':', mensagem.toString());
-    ultimaMensagem = mensagem.toString();
-    const [temperatura, umidade] = ultimaMensagem.split(':');
-    exibirTemperaturaNaPagina(temperatura);
-    exibirUmidadeNaPagina(umidade);
-    adicionarMensagemAoGrafico(temperatura);
-    const chaveTemperatura = adicionarTemperatura(temperatura);
-    console.log("Temperatura adicionada com sucesso. Chave gerada:", chaveTemperatura);
-});
-
-// Funções
-
-function exibirTemperaturaNaPagina(temperatura) {
-    const listaMensagens = document.getElementById('mensagens');
-    listaMensagens.innerHTML = '';
-    const novaMensagem = document.createElement('li');
-    novaMensagem.textContent = `${temperatura}ºC`;
-    listaMensagens.appendChild(novaMensagem);
-}
-
-function exibirUmidadeNaPagina(umidade) {
-    const listaMensagens = document.getElementById('mensagens-umidade');
-    listaMensagens.innerHTML = '';
-    const novaMensagem = document.createElement('li');
-    novaMensagem.textContent = `${umidade}%`;
-    listaMensagens.appendChild(novaMensagem);
-}
-
-ClienteMqtt.on('connect', function () {
-    console.log('Conectado ao broker MQTT raspberry/api');
-    
-    if (isConnected) {
-        console.log('Conectado ao broker MQTT para recebimento de dados');
+    function exibirUmidadeNaPagina(umidade) {
+        const listaMensagens = document.getElementById('mensagens-umidade');
+        if (listaMensagens) {
+            listaMensagens.innerHTML = '';
+            const novaMensagem = document.createElement('li');
+            novaMensagem.textContent = `${umidade}%`;
+            listaMensagens.appendChild(novaMensagem);
+        } else {
+            console.error('Elemento mensagens-umidade não encontrado.');
+        }
     }
-});
 
-ClienteMqtt.on('error', function (erro) {
-    console.error('Erro de conexão:', erro);
+    ClienteMqtt.on('connect', function () {
+        console.log('Conectado ao broker MQTT raspberry/api');
+        
+        if (isConnected) {
+            console.log('Conectado ao broker MQTT para recebimento de dados');
+        }
+    });
+
+    ClienteMqtt.on('error', function (erro) {
+        console.error('Erro de conexão:', erro);
+    });
 });
